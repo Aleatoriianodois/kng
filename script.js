2409cc12d871f5cb386281123e2a54b879dba532
Aleatoriianodois <avantikatekilla@hotmail.com>
2024-12-21 14:25:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const pessoasSerieA = [
+    {nome: "Mendes", titulos: "8", tiktok: "@mendesssssssssss.02", patente: "logo-patente-platina-4.png", batalhas: ["1"], imagem: "mendes.png"},
     {nome: "Paulo César", titulos: "7", tiktok: "@pcoficial_", patente: "logo-patente-prata-3.png", batalhas: ["1", "2", "3"], imagem: "paulocesar.png"},
-    {nome: "Mendes", titulos: "7", tiktok: "@mendesssssssssss.02", patente: "logo-patente-platina-3.png", batalhas: ["1"], imagem: "mendes.png"},
     {nome: "WTzangetsu🌻", titulos: "4", tiktok: "@wtzangetsu", patente: "logo-patente-platina-1.png", batalhas: ["1"], imagem: "zan.png"},
     {nome: "Jose Jeimesson", titulos: "4", tiktok: "@josejeimesson", patente: "logo-patente-prata-1.png", batalhas: ["1"], imagem: "jose.png"},
     {nome: "Trevor", titulos: "3", tiktok: "@trevor1339", patente: "none.png", batalhas: ["1"], imagem: "trevor1339.png"},
