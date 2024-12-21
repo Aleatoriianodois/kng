1fe6f41fc2c1d042db6d7db3368ed2cb54465f17
Aleatoriianodois <avantikatekilla@hotmail.com>
2024-12-20 21:21:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ const pessoasSerieA = [
     {nome: "Luyh Calistenia", titulos: "1", tiktok: "@luyh_sw", patente: "none.png", batalhas: ["1"], imagem: "luyh_sw.png"},
     {nome: "Gabriel", titulos: "1", tiktok: "@gabriel_vl7", patente: "none.png", batalhas: ["1"], imagem: "gabriel_vl7.png"},
     {nome: "Draken Lessa", titulos: "1", tiktok: "@draken.lessa", patente: "none.png", batalhas: ["1"], imagem: "draken.lessa.png"},
+    {nome: "abimaelmotadossan", titulos: "1", tiktok: "@abimaelmotadossantosmota", patente: "none.png", batalhas: ["1"], imagem: "abimaelmotadossantosmota.png"},
 ];
 
 const pessoasSerieB = [
